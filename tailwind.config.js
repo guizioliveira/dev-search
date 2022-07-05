0895158400cddaffc2bb08a2a5bd01efb601a17b
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-07-05 15:29:30 -0300

    feat(tailwindcss): create color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        apricot: "#F4A27E",
+        "colonial-white": "#FFE6AB",
+        charade: "#0D0D0E",
+        "outer-space": "#1F282C",
+        "oslo-gray": "#90969A",
+        iron: "#DADFE2",
+        gallery: "#EAEAEA",
+        lighter: "#FAFAFA",
+      },
+    },
   },
   plugins: [],
 };
