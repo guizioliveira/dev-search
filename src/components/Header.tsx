9b59695d2c1c35af5f732a0f128e5c94148cebea
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-07-05 16:48:17 -0300

    feat(components): adding responsivity to `Header` component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="h-80 w-full bg-outer-space">
-      <div className="container mx-auto flex items-center justify-between pt-14">
-        <h1 className="max-w-[237px] text-5xl font-extrabold uppercase leading-tight text-white">
+    <div className="h-[360px] w-full bg-outer-space md:h-80">
+      <div className="container mx-auto flex flex-col justify-between gap-3 px-3 pt-10 md:flex-row md:items-center md:gap-0 md:px-0 md:pt-14">
+        <h1 className="max-w-[168px] text-3xl font-extrabold uppercase leading-tight text-white md:max-w-[237px] md:text-5xl">
           Looking for a <span className="text-apricot">dev</span>?
         </h1>
-        <div className="flex flex-col items-end gap-3">
-          <h2 className="text-2xl font-semibold text-white">
+        <div className="flex justify-between gap-3 md:flex-col md:items-end md:justify-start">
+          <h2 className="text-sm font-semibold text-white md:text-2xl">
             Let us help you!
           </h2>
-          <div className="max-w-[430px] rounded-2xl bg-apricot py-4 px-8">
-            <span className="inline-block text-right text-lg text-white">
+          <div className="-mt-16 max-w-[150px] rounded-2xl bg-apricot p-4 md:mt-0 md:max-w-[430px] md:px-8">
+            <span className="inline-block text-xs text-white md:text-right md:text-lg">
               Search for developers and discover their work through the github!
             </span>
           </div>
