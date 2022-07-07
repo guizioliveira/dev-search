48b517a9985f2dd669e1ae553526929d995313b6
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-07-06 22:06:42 -0300

    feat(components): set state to deal with the modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import { Card } from "./components/Card";
 import { Header } from "./components/Header";
+import { Modal } from "./components/Modal";
 import { SearchBar } from "./components/SearchBar";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
   return (
     <>
       <Header />
       <SearchBar />
       <Card />
+      <Modal isModalOpen={isModalOpen} />
     </>
   );
 }
