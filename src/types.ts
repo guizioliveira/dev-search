609d501dc31adacd8b8965dde8b4475a05c62a9e
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-07-07 03:13:20 -0300

    feat(components): creating `GithubUser` and `Repository` types

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export type GithubUser = {
+  login: string;
+  name: string;
+  avatar_url: string;
+  html_url: string;
+  company: string;
+  bio: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  location: string;
+  created_at: string;
+  repositories: Array<Repository>;
+};
+
+export type Repository = {
+  id: number;
+  name: string;
+  description: string;
+  language: string;
+  stargazers_count: number;
+};
