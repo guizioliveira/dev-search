e1d11db787415087aa8634704146170db812a7ba
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-07-10 01:37:00 -0300

    feat(components): change `dateFormatter` day format

--- src/common/formaters.ts
+++ src/common/formaters.ts
@@ -5,9 +5,13 @@ export function formatter(info: string | null) {
 export function dateFormatter(date: string) {
   const convertedDate = new Date(date);
   const formatedDate = convertedDate.toLocaleDateString("en-US", {
-    day: "numeric",
+    day: "2-digit",
     month: "short",
     year: "numeric",
   });
   return formatedDate;
+}
+
+export function shortenHash(hash: string) {
+  return hash.substring(0, 7);
 }
