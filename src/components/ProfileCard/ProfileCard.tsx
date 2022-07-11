efccf83c86408a163dda3eb9555ff073703e7690
Guilherme de Oliveira <guizi_oliveira@hotmail.com>
2022-07-10 22:17:36 -0300

    feat(components): adding margin bottom to `ProfileCard`

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -27,7 +27,7 @@ export const ProfileCard = ({
   }
 
   return (
-    <div className="container mx-auto mt-8 px-3 md:mt-14 md:px-0">
+    <div className="container mx-auto my-8 px-3 md:my-14 md:px-0">
       <div className="w-full rounded-t-2xl bg-iron px-4 pt-6 md:px-8">
         <div className="flex flex-col md:flex-row md:items-center md:gap-8">
           <div className="flex gap-3 md:items-center md:gap-8">
